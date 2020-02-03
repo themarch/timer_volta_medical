e609a17510881f95b762f816ba63c7e8ed0ee2f0
themarch <39468266+themarch@users.noreply.github.com>
2020-02-03 13:22:14 +0100

    Update server.py

--- back-end/server.py
+++ back-end/server.py
@@ -12,7 +12,7 @@ async def timer_send(websocket, port):
             await websocket.send(time_send)
             await asyncio.sleep(1)
     except Exception:
-        print(f"Code 1001, hard disconnect when refresh React")
+        print("Code 1001, hard disconnect when refresh React")
 
 start_server = websockets.serve(timer_send, 'localhost', 1234)
 asyncio.get_event_loop().run_until_complete(start_server)
