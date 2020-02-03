74cad2c06a9576370ac09bc0b0e583b86ce08378
themarch <39468266+themarch@users.noreply.github.com>
2020-02-03 18:28:57 +0100

    Update server.py

--- back-end/server.py
+++ back-end/server.py
@@ -8,11 +8,11 @@ async def timer_send(websocket, port):
     try:
         while True:
             time_send = time.strftime('%X')
-            time_send = str(time_send)
+            time_send = str(time_send) #cast in str. Else React can't print.
             await websocket.send(time_send)
             await asyncio.sleep(1)
     except Exception:
-        print("Code 1001, hard disconnect when refresh React")
+        print("Code 1001, hard disconnect when refresh React") #When refresh react page, error 1001 arrive
 
 start_server = websockets.serve(timer_send, 'localhost', 1234)
 asyncio.get_event_loop().run_until_complete(start_server)
